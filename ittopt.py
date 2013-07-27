15037237404402c31ebb965e6b52423071e763d7
Matt Redmond <mttrdmnd@gmail.com>
2013-07-27 10:10:41 +0200

    slight modifications

--- ittopt.py
+++ ittopt.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, url_for, render_template 
 from stravalib import *
+import tempfile
 DEBUG = True
 
 app = Flask(__name__)
@@ -11,53 +12,48 @@ def show_mainpage():
 
 @app.route('/optimize', methods=['POST'])
 def optimize():
-  url = request.form["rideurl"]
   try:
-    segmentid = int(url[url.rindex('/')+1:])
-    weight = float(request.form["weight"])
-    cda = float(request.form["frontalcda"])
-    wind_direction = float(request.form["winddirection"])
-    wind_velocity = float(request.form["windvelocity"])
-    resolution = float(request.form["resolution"])
-    power_30 = float(request.form["30sPower"])
-    power_60 = float(request.form["1mPower"])
-    power_300 = float(request.form["5mPower"])
-    power_600 = float(request.form["10mPower"])
-    power_1200 = float(request.form["20mPower"])
-    power_3600 = float(request.form["60mPower"])
+    url = request.form["rideurl"]
+    model_params["num_waypoints"] = float(request.form["resolution"])
+    
+    model_params = {}
+    model_params["rider"] = {}
+    model_params["rider"]["weight"] = float(request.form["weight"])
+    model_params["rider"]["cda"]  = float(request.form["cda"])
+    model_params["rider"]["crr"] = float(request.form["crr"])
+    model_params["environ"] = {}
+    model_params["environ"]["temp"] = float(request.form["temp"])
+    model_params["environ"]["dewpoint"] = float(request.form["dewpoint"])
+    model_params["environ"]["pressure"] = float(request.form["pressure"])
+    model_params["environ"]["wind_direction"] = float(request.form["winddirection"])
+    model_params["environ"]["wind_velocity"] = float(request.form["windvelocity"])
+    model_params["power"] = {}
+    model_params["power"][30] = float(request.form["30sPower"])
+    model_params["power"][60] = float(request.form["1mPower"])
+    model_params["power"][300] = float(request.form["5mPower"])
+    model_params["power"][600] = float(request.form["10mPower"])
+    model_params["power"][1200] = float(request.form["20mPower"])
+    model_params["power"][3600] = float(request.form["60mPower"])
   except ValueError:
     return render_template('index.html', console="Try using numerical values, silly.")
-  
-  rank = 0
-  while True:
-    if rank > 6:
-      return render_template('index.html', console="Unable to obtain segment data after checking top 6 riders, perhaps something is borked?")
-    try:
-      rideid = identify_rideid_of_rank_k(segmentid, rank)
-      efforts = get_efforts_from_ride(rideid)
-      segment_details = get_segment_from_efforts(efforts, segmentid)
-      full_stream_latlng = get_stream_latlng(rideid)
-      segment_latlng = get_segment_latlng(full_stream_latlng, segment_details)
-      compressed_polyline = ramerdouglas(segment_latlng, dist=resolution)
-      latlngalt = append_elevation(compressed_polyline)
-      break
-    except Exception:
-      print "Bad data on rider of rank %d" % rank
-      rank += 1
-      continue
-  #except Exception:
-  #  return render_template('index.html', console="An unexpected error occurred while trying to load data for this segment.")
-  if len(latlngalt) <= 2:
-    return render_template('index.html', console="Unable to obtain compressed segment data.")
-  
-  else:
-    # Do the rendering on a new page
-    line_segs = ""
-    for i in range(len(latlngalt)-1):
-      line_segs += "%s --> %s \n" % (str(latlngalt[i]), str(latlngalt[i+1]))
-    
-    return render_template('results.html', console=line_segs, mapcoords=str(compressed_polyline))
 
+  path_to_experiment = BuildExperimentDirectory(url, model_params)
+  results = ProcessExperimentDirectory(path_to_experiment)
+  return RenderResults(results)
+
+def BuildExperimentDirectory(url):
+  # Construct a folder with all of the correct parameter files.
+  path = tempfile.mkdtemp()
+  DownloadCourseKML(url, path)
+  WriteParameterFile(weight, cda, crr, temp, dewpoint, pressure, wind_direction, wind_velocity,
+                     num_waypoints
+  return path
+
+def ProcessExperimentDirectory(path):
+  # Load values into optimizer and optimize!
+  pass
+def RenderResults(results):
+  return render_template('results.html', console="Foobar!", mapcoords=dsfho)
 
 if __name__ == '__main__':
   app.run(debug=True)
